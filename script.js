6e87cb69b75971c37427da01e09968f1247e0dde
Tango301 <vedanthshbhat@gmail.com>
2026-03-19 12:47:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ let d = hello;
 console.log(d);
 
 alert('hello')
+
+function decorator(wrap, func) {
+  return (...para) => {
+    func(para)
+    return wrap(para)
+  }
+}
+
+const mylog = decorator(window.console.log, (...para)=>{
+  const ul = document.querySelector(`ul[class=msg]`)
+  const range = document.createRange()
+  const frag = range.createContextualFragment(`<li>${para}</li>`)
+  ul.append(frag)
+})
+
+mylog("Hello world!")
